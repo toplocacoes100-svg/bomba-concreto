42f7c62af74b6906c02a2130ecb103584d964f95
toplocacoes100-svg <toplocacoes100@gmail.com>
2026-09-20 16:20:28 -0300

    Update main.jsx12

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { storage } from "./firebase.js";
 import App from "./App.jsx";
+
+// O App.jsx lê e grava tudo via window.storage. Aqui ligamos esse "atalho"
+// ao Firebase (Firestore) antes de o app iniciar.
+window.storage = storage;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
